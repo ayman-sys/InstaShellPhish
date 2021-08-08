4abf09df6a62050ffdf0b1b5a8a96f3bd93e0929
Ayman Shaikh <67065247+ayman-sys@users.noreply.github.com>
2021-08-08 18:01:46 +0530

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # ShellPhish v2.5-Mod
-# Moded by @AbirHasan2005
+# Moded by @AAyman.codes
 
-# Join Telegram Group for help: http://t.me/linux_repo
+
 
 
 trap 'printf "\n";stop;exit 1' 2
@@ -71,13 +71,13 @@ clear
 printf "\n"
 printf "\e[1;92m       __  _             _  _           _               _      \e[0m\n"
 printf "\e[1;92m      / / | | v2.5-MOD  | || | ______  | |     _       | |     \e[0m\n"
-printf "\e[1;92m     / /  | |           | || |(_____ \ | |    (_)      | |     \e[0m\n"
+printf "\e[1;92m     / /  | | By Ayman  | || |(_____ \ | |    (_)      | |     \e[0m\n"
 printf "\e[1;91m     \ \  | |___   ____ | || | _____) )| |___  _   ___ | |___  \e[0m\n"
 printf "\e[1;91m      \ \ | |_  | / _  )| || ||  ____/ | |_  || | /___)| |_  | \e[0m\n"
 printf "\e[1;92m  _____) )| | | |( (/ / | || || |      | | | || ||___ || | | | \e[0m\n"
 printf "\e[1;92m (______/ |_| |_| \____)|_||_||_|      |_| |_||_|(___/ |_| |_| \e[0m\n"
 printf "\n"
-printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @AbirHasan2005 \e[0m\e[1;93m.:.:.\e[0m\n\n"
+printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @ayman.codes \e[0m\e[1;93m.:.:.\e[0m\n\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
 printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by ShellPhish ::\e[0m\n"
 printf " \n"
